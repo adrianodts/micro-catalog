bd48b52027691fd0067595da7b77c830a7f8f92f
Adriano Dantas <adrianodts@msn.com>
2021-04-21 19:50:06 -0300

    commit inicial

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,7 @@
 import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
 import {inject} from '@loopback/context';
+import { CategoryRepository } from '../repositories';
+import { repository } from '@loopback/repository';
 
 /**
  * OpenAPI response for ping()
@@ -32,7 +34,10 @@ const PING_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class PingController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+  constructor(
+    @inject(RestBindings.Http.REQUEST) private req: Request,
+    @repository(CategoryRepository) private categoryRepo: CategoryRepository
+  ) {}
 
   // Map to `GET /ping`
   @get('/ping', {
@@ -48,5 +53,15 @@ export class PingController {
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
     };
+  }
+
+  @get('/categories')
+  index() {
+    this.categoryRepo.create({
+      id: '1',
+      name: 'Product 1',
+      description: 'This product is very useful.'
+    });
+    return this.categoryRepo.find();
   }
 }
---
+++ src/datasources/esv7.datasource.ts
@@ -0,0 +1,53 @@
+import {inject, lifeCycleObserver, LifeCycleObserver, ValueOrPromise} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'esv7',
+  connector: 'esv6',
+  index: 'catalog',
+  apiVersion: '7',
+  configuration: { 
+    node: process.env.ELASTIC_SEARCH_HOST,
+    requestTimeout: process.env.ELASTIC_SEARCH_REQUEST_TIMEOUT,
+    pingTimeout: process.env.ELASTIC_SEARCH_PING_TIMEOUT
+  },
+  mappingProperties: {
+
+  },
+  defaultSize: 50,
+  indexSettings: {
+    number_of_shards: 2,
+    number_of_replicas: 1
+  },
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class Esv7DataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'esv7';
+  // static readonly defaultConfig = config;
+  // @inject('datasources.config.esv7', {optional: true})
+  // dsConfig: object = config,
+  constructor() {
+    super(config);
+  }
+
+  /**
+   * Start the datasource when application is started
+   */
+   start(): ValueOrPromise<void> {
+    // Add your logic here to be invoked when the application is started
+  }
+
+  /**
+   * Disconnect the datasource when application is stopped. This allows the
+   * application to be shut down gracefully.
+   */
+  stop(): ValueOrPromise<void> {
+    return super.disconnect();
+  }
+}
